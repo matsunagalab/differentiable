0f93182d75fcb36864ec20a4a7a8bbd454e1a784
ysako <y.sako.468@ms.saitama-u.ac.jp>
2023-12-01 13:27:40 +0900

    back up alanine_dipeptide

---
+++ forcefield/alanine_dipeptide/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p all
+#SBATCH -J sim_target # job name
+#SBATCH -n 1  # num of total mpi processes
+#SBATCH -c 1  # num of threads per mpi processes
+#SBATCH -o run.log
+#SBATCH -w n4
+
+#python sim.py alanine-dipeptide-nowater.pdb GB99dms_new.xml traj_new
+python sim.py alanine-dipeptide-nowater.pdb GB99dms.xml traj_target 5000000
