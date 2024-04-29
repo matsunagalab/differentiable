fadd9330395070e94a7286b352b6fbbda2d16144
ysako <y.sako.468@ms.saitama-u.ac.jp>
2024-04-29 17:55:01 +0900

    fix tram.jl

--- forcefield/tram/tram.jl
+++ forcefield/tram/tram.jl
@@ -6,6 +6,7 @@ Estimates the free energy differences by transition-based reweighting analysis m
 ## Arguments
 - `c_ijk::AbstractArray`: Array containing the counts of transitions from state `i` to state `j` in ensemble `k`.
 - `N_ik::AbstractArray`: Array containing the counts of samples in state `i` in ensemble `k`.
+- `N_k::AbstractArray`: Array containing the counts of samples in ensemble `k`.
 - `b_kln::AbstractArray`: Array of bias factors of state `k` evaluated at state `l`.
 - `index_of_cluster_kn::AbstractArray`: Array fo index of the state to which the nth sample of ensemble `k` belongs.. 
 
@@ -13,30 +14,89 @@ Estimates the free energy differences by transition-based reweighting analysis m
 - `tol::Real = 1e-10`: Tolerance value for convergence criteria.
 - `max_iter::Integer = 20`: Maximum number of iterations allowed.
 """
-function tram(c_ijk, N_ik, b_kln, index_of_cluster_kn; tol=1e-10, max_iter=20)
+function tram(c_ijk, N_ik, N_k, b_kln, index_of_cluster_kn; tol=1e-10, max_iter=100)
     f_ik, v_ik, R_ik = tram_init(c_ijk, N_ik)
 
     for i in 1:max_iter
-        R_ik = tram_R_ik(c_ijk, v_ik, f_ik, N_ik)
         for j in 1:100 #この100はなんとなくの値, f_ikの更新に比べ計算量が少ないため適当に大きい値を設定している
-            v_ik = tram_v_new_ik(c_ijk, v_ik, f_ik)
+            v_new_ik = tram_v_new_ik(c_ijk, v_ik, f_ik)
+            #=
+            if(j == 100)
+                println("delta = $(maximum(abs.(v_ik .- v_new_ik)))")
+                v_ik = v_new_ik
+            end
+            =#
+            v_ik = v_new_ik
         end
-        f_new_ik = tram_f_new_ik(f_ik, R_ik, b_kln, N_ik, index_of_cluster_kn)
-
-        max_dif = maximum(f_ik .- f_new_ik)
-        println("iteration = $(i), delta = $(max_dif)")
-        if(max_dif < tol)
-            f_ik = f_new_ik
-            break
+        
+        R_ik = tram_R_ik(c_ijk, v_ik, f_ik, N_ik)
+        f_new_ik = tram_f_new_ik(f_ik, R_ik, b_kln, N_k, index_of_cluster_kn)
+        max_dif = maximum(abs.(f_ik .- f_new_ik))
+        if i % (max_iter/10) == 1 || i == max_iter 
+            println("iteration = $(i), delta = $(max_dif)")
         end
         f_ik = f_new_ik
+        if(max_dif < tol)
+            println("iteration = $(i), delta = $(max_dif)")
+            break
+        end
     end
     
-    for j in 1:100 # v_ikも一応更新する
-        v_ik = tram_v_new_ik(c_ijk, v_ik, f_ik)
+    return f_ik, v_ik, R_ik
+end
+
+function tram_by_delta(c_ijk, N_ik, N_k, b_kln, index_of_cluster_kn; tol=1e-10, max_iter=10000)
+    f_ik, v_ik, R_ik = tram_init(c_ijk, N_ik)
+
+    for i in 1:max_iter
+        for j in 1:100 #この100はなんとなくの値, f_ikの更新に比べ計算量が少ないため適当に大きい値を設定している
+            v_new_ik = tram_v_new_ik(c_ijk, v_ik, f_ik)
+            #=
+            if(j == 100)
+                println("delta = $(maximum(abs.(v_ik .- v_new_ik)))")
+                v_ik = v_new_ik
+            end
+            =#
+            v_ik = v_new_ik
+        end
+        
+        R_ik = tram_R_ik(c_ijk, v_ik, f_ik, N_ik)
+        #f_new_ik = tram_f_new_ik(f_ik, R_ik, b_kln, N_k, index_of_cluster_kn)
+        f_new_ik = similar(f_ik)
+        δ_i = tram_δ_i(c_ijk, v_ik, f_ik)
+        for j in 1:m
+            f_new_ik[j, :] = f_ik[j, :] .+ δ_i[j]
+        end
+        
+        max_dif = maximum(abs.(f_ik .- f_new_ik))
+        if i % (max_iter/10) == 1 || i == max_iter 
+            println("iteration = $(i), delta = $(max_dif)")
+        end
+        f_ik = f_new_ik
+        if(max_dif < tol)
+            println("iteration = $(i), delta = $(max_dif)")
+            break
+        end
+    end
+    
+    return f_ik, v_ik, R_ik
+end
+
+function tram_δ_i(c_ijk, v_ik, f_ik)
+    m, K = size(v_ik)
+
+    δ_i = zeros(Float64, m)
+    for i in 1:m
+        x = 0
+        for k in 1:K
+            for j in 1:m
+                x += (c_ijk[i, j, k] + c_ijk[j, i, k]) * v_ik[j, k] / (v_ik[j, k] + exp(f_ik[i, k] - f_ik[j, k]) * v_ik[i, k])
+            end
+        end
+        δ_i[i] = log(x) - log(sum(c_ijk[:, i, :]))
     end
 
-    return f_ik, v_ik, R_ik
+    return δ_i
 end
 
 function tram_p_ijk(c_ijk, v_ik, f_ik)
@@ -88,44 +148,33 @@ end
 
 # f^k,newの計算では、Σ_(x∈X_i)があるため、index_of_cluster_knを用いる
 # b_klnはkアンサンブルをl番目のポテンシャルで評価したもの
-
-function tram_f_new_ik(f_ik, R_ik, b_kln, N_ik, index_of_cluster_kn)
-    m, K = size(N_ik)
+function tram_f_new_ik(f_ik, R_ik, b_kln, N_k, index_of_cluster_kn)
+    m, K = size(f_ik)
     f_new_ik = zeros(Float64, m, K)
 
-    log_w_ikn = tram_log_w_ikn(f_ik, R_ik, b_kln, N_ik, index_of_cluster_kn)
     for k in 1:K
         for i in 1:m
-            tmp = Array{Float64}(undef, 0)
-            for n in 1:length(index_of_cluster_kn[k, :])
-                if(index_of_cluster_kn[k, n] == i)
-                    append!(tmp, log_w_ikn[i, k, n])
-                end
-            end
-            f_new_ik[i, k] = - MDToolbox.logsumexp(tmp)
+            log_wik_jn = tram_log_wik_jn(f_ik, R_ik, b_kln, N_k, i, k)
+            log_wik = log_wik_jn[index_of_cluster_kn .== i]
+            f_new_ik[i, k] = - MDToolbox.logsumexp(log_wik)
         end
     end
-    
+
     f_new_ik = normalize_f_ik(f_new_ik)
     return f_new_ik
 end
 
-function tram_log_w_ikn(f_ik, R_ik, b_kln, N_ik, index_of_cluster_kn)
-    m, K = size(R_ik)
-    log_w_ikn = zeros(Float64, m, K, size(b_kln, 3))
+# K×N_maxの配列で、f_ikの更新に用いる
+# j行目n列目はjアンサンブルのn個目のデータを用い更新する
+function tram_log_wik_jn(f_ik, R_ik, b_kln, N_k, istate, kstate)
+    m, K = size(f_ik)
+    log_wik_jn = zeros(Float64, (K, maximum(N_k)))
 
     for k in 1:K
-        for i in 1:m
-            for n in 1:size(b_kln, 3)
-                if(index_of_cluster_kn[k, n] == i)
-                    x = log.(R_ik[i, :]) + f_ik[i, :] - (b_kln[k, :, n] .- b_kln[k, k, n])
-                    log_w_ikn[i, k, n] = - MDToolbox.logsumexp(x)
-                end
-            end
-        end
+        x = repeat(log.(R_ik[istate, :]), 1, N_k[k]) .+ repeat(f_ik[istate, :], 1, N_k[k]) .- (b_kln[k, :, 1:N_k[k]] .- repeat(b_kln[k:k, kstate, 1:N_k[k]], K, 1))
+        log_wik_jn[k:k, 1:N_k[k]] .= - logsumexp_over_row(x)
     end
-    
-    return log_w_ikn
+    return log_wik_jn
 end
 
 # k=1に対してΣ_i(exp(-f_ik))=1
@@ -165,4 +214,11 @@ function tram_init(c_ijk, N_ik)
     R_ik = tram_R_ik(c_ijk, v_ik, f_ik, N_ik)
 
     return f_ik, v_ik, R_ik
+end
+
+function logsumexp_over_row(x)
+    max_x = maximum(x, dims=1)
+    exp_x = exp.(x .- max_x)
+    s = log.(sum(exp_x, dims=1)) .+ max_x
+    return s
 end
